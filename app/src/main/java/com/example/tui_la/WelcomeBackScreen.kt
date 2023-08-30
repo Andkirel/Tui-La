3ed05c96ecec7618f2a84e9042518b7fbcdd3ae3
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2023-08-30 12:46:28 -0500

    Created layout for WelcomeBackScreen and .kt to load the screen and run the progress bar animation. Current intent set to FirstFragment until the "How are you feeling?" screen is created. Added TODO and tentative code for determining if user is new/return user & update this screen accordingly (requires Firebase implementation)

--- app/src/main/java/com/example/tui_la/WelcomeBackScreen.kt
+++ app/src/main/java/com/example/tui_la/WelcomeBackScreen.kt
@@ -10,9 +10,27 @@ class WelcomeBackScreen : AppCompatActivity() {
     private var progressBarStatus = 0
     private var loadInt:Int = 0
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.screen_welcome_welcomeback)
+
+        /*TODO: add code to determine if new or returning user. Depending on answer, adjust WelcomeBackText to say "Welcome".
+           Also adjust the usernameText to the correct username.*/
+        /* Tentative code?
+        private val userRef = db.collection("user").document("userID")
+        userRef.get()
+          .addOnSuccessListener { document ->
+            if (document != null) {
+                Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+            } else {
+                Log.d(TAG, "No such document")
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
+    */
 
         val progressBar = findViewById<ProgressBar>(R.id.welcomeScreenProgressBar)
         Thread(Runnable {
