d6ca6307e1c77ddd4e4e8b701c7cce0eff3d876d
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2023-10-31 09:55:33 -0500

    Did some code cleanup - Renaming Variables for clarity, removing unused code, etc.

--- app/src/main/java/com/example/tui_la/GuidedMeditationExoPlayer.kt
+++ app/src/main/java/com/example/tui_la/GuidedMeditationExoPlayer.kt
@@ -24,7 +24,6 @@ import androidx.media3.datasource.HttpDataSource.InvalidResponseCodeException
 import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.RenderersFactory
-import androidx.media3.exoplayer.source.ConcatenatingMediaSource2
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
 import androidx.media3.exoplayer.source.MergingMediaSource
 import androidx.media3.exoplayer.source.ProgressiveMediaSource
@@ -54,7 +53,7 @@ import java.nio.charset.StandardCharsets
 import javax.net.ssl.HttpsURLConnection
 
 
-//!!DO NOT CHANGE SC_CLIENT_ID OR SC_CLIENT_SECRET.They are necessary to access the SoundCloud API.!!//
+//!!DO NOT CHANGE THE BELOW STRINGS. They are necessary to access the SoundCloud API AND Google Drive APIs.!!//
 private const val SC_Client_ID = "7mveilIe54NH0aS3LBqBdkJuyo8CCvIu"
 private const val SC_Client_Secret = "gdzGo4IE4O3AmipQwvu6TGDE52sKBiJY"
 private const val Google_Cloud_API_Key = "AIzaSyA6n51QSYLze9UusF2luIrpk3momzDcm3w"
@@ -84,10 +83,6 @@ private const val TAG = "PlayerActivity"
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
     private lateinit var listener: ValueEventListener
-
-    companion object {
-        private const val REQUEST_SIGN_IN = 1
-    }
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -136,7 +131,6 @@ private const val TAG = "PlayerActivity"
             }
         })*/
         setupPlayer()
-        //requestSignIn()
         runBlocking{ googleDriveAuthorization() }
         streamMusic()
         //runBlocking { postAuthorization() }
@@ -166,25 +160,25 @@ private const val TAG = "PlayerActivity"
         GoogleSignIn.getLastSignedInAccount(myCredential.context)
 
         GlobalScope.launch(Dispatchers.IO) {
-            val authUrl = URL("https://accounts.google.com/o/oauth2/v2/auth")
-            val authHttpsURLConnection = authUrl.openConnection() as HttpsURLConnection
-            authHttpsURLConnection.setRequestProperty("client_id", Tui_La_Service_OAuth2_Client_ID)
-            authHttpsURLConnection.setRequestProperty("redirect_uri",
+            val authUrlGoogle = URL("https://accounts.google.com/o/oauth2/v2/auth")
+            val authGoogleURLConnection = authUrlGoogle.openConnection() as HttpsURLConnection
+            authGoogleURLConnection.setRequestProperty("client_id", Tui_La_Service_OAuth2_Client_ID)
+            authGoogleURLConnection.setRequestProperty("redirect_uri",
                 "https://www.googleapis.com/drive/v3/drives")
-            authHttpsURLConnection.setRequestProperty("response_type","code")
-            authHttpsURLConnection.setRequestProperty("scopes","https://www.googleapis.com/auth/drive")
-            authHttpsURLConnection.setRequestProperty(
+            authGoogleURLConnection.setRequestProperty("response_type","code")
+            authGoogleURLConnection.setRequestProperty("scopes","https://www.googleapis.com/auth/drive")
+            authGoogleURLConnection.setRequestProperty(
                 "Authorization: Bearer",
                 Tui_La_Service_OAuth2_Client_ID
             )
-            authHttpsURLConnection.setRequestProperty("Accept", "application/json")
-            authHttpsURLConnection.doInput = true
-            authHttpsURLConnection.doOutput = true
+            authGoogleURLConnection.setRequestProperty("Accept", "application/json")
+            authGoogleURLConnection.doInput = true
+            authGoogleURLConnection.doOutput = true
             //val sslConnection = authUrl.openConnection() as SSLConnectionSocketFactory
 
             //Check if connection is successful
-            val responseCode = authHttpsURLConnection.responseCode
-            if (responseCode == HttpURLConnection.HTTP_OK) {
+            val responseCodeGoogleAuth = authGoogleURLConnection.responseCode
+            if (responseCodeGoogleAuth == HttpURLConnection.HTTP_OK) {
                 //Folder ID: 1S2sGWvIeG3kKtECt200Q92WwgmXnEvZX
                 //File ID: 1RMdKtJUQoYA4t1KfycAuq24QizkKemx7
                 //val url = URL("https://www.googleapis.com/drive/v3/folders/1S2sGWvIeG3kKtECt200Q92WwgmXnEvZX HTTP/1.1")
@@ -192,36 +186,26 @@ private const val TAG = "PlayerActivity"
 
                 //authHttpsURLConnection.url.sameFile(url)
                 //val httpsURLConnection = url.openConnection() as HttpsURLConnection
-                authHttpsURLConnection.url.sameFile(fileUrl)
-                authHttpsURLConnection.connect()
-                authHttpsURLConnection.requestMethod = "GET"
+                authGoogleURLConnection.url.sameFile(fileUrl)
+                authGoogleURLConnection.connect()
+                authGoogleURLConnection.requestMethod = "GET"
 
-                val responseCode1 = authHttpsURLConnection.responseCode
-                if (responseCode1 == HttpURLConnection.HTTP_OK) {
-                    val reader = authHttpsURLConnection.inputStream.bufferedReader()
+                val responseCodeGoogleGetFile = authGoogleURLConnection.responseCode
+                if (responseCodeGoogleGetFile == HttpURLConnection.HTTP_OK) {
+                    val reader = authGoogleURLConnection.inputStream.bufferedReader()
                     val response1 = reader.readText()
                     val jsonObject1 = Gson().toJson(response1).toString()
-                    var vidMediaMeta : String = ""
 
-                    Log.i("Response Message: ",responseCode1.toString())
-                    //Log.i("Video Media Metadata: ", jsonObject1)
-                    //Log.i("Drive : ", jsonObject1)
+                    Log.i("Response Message: ",responseCodeGoogleGetFile.toString())
                     reader.close()
-                    authHttpsURLConnection.disconnect()
+                    authGoogleURLConnection.disconnect()
                 } else {
-                    Log.e("HTTPS Drives Connection Error", responseCode1.toString())
-                    Log.e("ERROR MESSAGE", authHttpsURLConnection.responseMessage)
+                    Log.e("Google Drive Connection Error", responseCodeGoogleGetFile.toString())
+                    Log.e("ERROR MESSAGE", authGoogleURLConnection.responseMessage)
                 }
             } else {
-                Log.e("Authorization Connection Error", responseCode.toString())
-                Log.e("ERROR MESSAGE", authHttpsURLConnection.responseMessage)
-                if (responseCode == 429) { //Cannot ask for more access tokens because hit the rate_limit
-                    val reader = authHttpsURLConnection.inputStream.bufferedReader()
-                    val response = reader.readText()
-                    Log.d("RATE LIMIT:", response)
-                    reader.close()
-                    authHttpsURLConnection.disconnect()
-                }
+                Log.e("Authorization Connection Error", responseCodeGoogleAuth.toString())
+                Log.e("ERROR MESSAGE", authGoogleURLConnection.responseMessage)
             }
         }
 
@@ -238,28 +222,28 @@ private const val TAG = "PlayerActivity"
         val postData=params.toByteArray(StandardCharsets.UTF_8)
 
         GlobalScope.launch(Dispatchers.IO){
-            val url= URL("https://api.soundcloud.com/oauth2/token")
-            val httpsURLConnection = url.openConnection() as HttpsURLConnection
-            httpsURLConnection.requestMethod="POST"
-            httpsURLConnection.setRequestProperty("grant_type","client_credentials")
-            httpsURLConnection.setRequestProperty("client_id",SC_Client_ID)
-            httpsURLConnection.setRequestProperty("client_secret",SC_Client_Secret)
-            httpsURLConnection.setRequestProperty("Content-Type","application/x-www-form-urlencoded") // The format of the content we're sending to the server. Confirmed via SoundCloud Public API Specification.
-            httpsURLConnection.setRequestProperty("accept","application/json;charset=utf-8") // The format of response we want to get from the server. Confirmed via SoundCloud Public API Specification.
-            httpsURLConnection.doInput=true
-            httpsURLConnection.doOutput=true
-            val dataOutputStream = DataOutputStream(httpsURLConnection.outputStream)
+            val authUrlSC = URL("https://api.soundcloud.com/oauth2/token")
+            val httpsURLConnectionSC = authUrlSC.openConnection() as HttpsURLConnection
+            httpsURLConnectionSC.requestMethod="POST"
+            httpsURLConnectionSC.setRequestProperty("grant_type","client_credentials")
+            httpsURLConnectionSC.setRequestProperty("client_id",SC_Client_ID)
+            httpsURLConnectionSC.setRequestProperty("client_secret",SC_Client_Secret)
+            httpsURLConnectionSC.setRequestProperty("Content-Type","application/x-www-form-urlencoded") // The format of the content we're sending to the server. Confirmed via SoundCloud Public API Specification.
+            httpsURLConnectionSC.setRequestProperty("accept","application/json;charset=utf-8") // The format of response we want to get from the server. Confirmed via SoundCloud Public API Specification.
+            httpsURLConnectionSC.doInput=true
+            httpsURLConnectionSC.doOutput=true
+            val dataOutputStream = DataOutputStream(httpsURLConnectionSC.outputStream)
             dataOutputStream.write(postData)
             dataOutputStream.flush()
 
             // Check if connection is successful
-            val responseCode= httpsURLConnection.responseCode
-            if(responseCode == HttpURLConnection.HTTP_OK){
+            val responseCodeSoundCloudAuth= httpsURLConnectionSC.responseCode
+            if(responseCodeSoundCloudAuth == HttpURLConnection.HTTP_OK){
                 // Get token database references
                 val accessTokenReference = database.getReference("SoundCloud Access Token/access_token")
                 val refreshTokenReference = database.getReference("SoundCloud Access Token/refresh_token")
                 // Get returned stream, parse, and place in jsonObject
-                val reader=httpsURLConnection.inputStream.bufferedReader()
+                val reader=httpsURLConnectionSC.inputStream.bufferedReader()
                 val response=reader.readText()
                 val jsonObject = JSONObject(response)
 
@@ -272,17 +256,17 @@ private const val TAG = "PlayerActivity"
                 refreshTokenReference.setValue(jsonObject.getString("refresh_token"))
                 // Close and disconnect
                 reader.close()
-                httpsURLConnection.disconnect()
+                httpsURLConnectionSC.disconnect()
             }
             else{
-                Log.e("HTTPURLCONNECTION_ERROR",responseCode.toString())
-                Log.e("ERROR MESSAGE",httpsURLConnection.responseMessage)
-                if(responseCode==429){ // Cannot ask for more access tokens because hit the rate_limit
-                    val reader=httpsURLConnection.inputStream.bufferedReader()
+                Log.e("SoundCloud Auth Connection Error",responseCodeSoundCloudAuth.toString())
+                Log.e("ERROR MESSAGE",httpsURLConnectionSC.responseMessage)
+                if(responseCodeSoundCloudAuth==429){ // Cannot ask for more access tokens because hit the rate_limit
+                    val reader=httpsURLConnectionSC.inputStream.bufferedReader()
                     val response=reader.readText()
                     Log.d("RATE LIMIT:",response)
                     reader.close()
-                    httpsURLConnection.disconnect()
+                    httpsURLConnectionSC.disconnect()
                 }
             }
 
@@ -291,22 +275,22 @@ private const val TAG = "PlayerActivity"
 
     private fun getStreamingTrack(){
         GlobalScope.launch(Dispatchers.IO){
-            val trackUrl = URL("https://api.soundcloud.com/tracks/${trackId}/streams")
-            var trackConn = trackUrl.openConnection() as HttpsURLConnection
-            trackConn.requestMethod = "GET"
-            trackConn.setRequestProperty("accept","application/json; charset=utf-8")
-            trackConn.setRequestProperty("Authorization","OAuth $accessToken")
+            val audioUrlSC = URL("https://api.soundcloud.com/tracks/${trackId}/streams")
+            var audioConnectionSC = audioUrlSC.openConnection() as HttpsURLConnection
+            audioConnectionSC.requestMethod = "GET"
+            audioConnectionSC.setRequestProperty("accept","application/json; charset=utf-8")
+            audioConnectionSC.setRequestProperty("Authorization","OAuth $accessToken")
 
-            trackConn.doInput = true
-            trackConn.doOutput = false
+            audioConnectionSC.doInput = true
+            audioConnectionSC.doOutput = false
 
             //Check if connection is successful
-            val responseCode = trackConn.responseCode
-            if(responseCode == HttpURLConnection.HTTP_OK){
-                val response= trackConn.inputStream.bufferedReader().readText()
+            val responseCodeAudioConnection = audioConnectionSC.responseCode
+            if(responseCodeAudioConnection == HttpURLConnection.HTTP_OK){
+                val response= audioConnectionSC.inputStream.bufferedReader().readText()
                 val jsonObject= JSONTokener(response).nextValue() as JSONObject
                 httpStreamUrl = jsonObject.getString("http_mp3_128_url")
-                trackConn.disconnect()
+                audioConnectionSC.disconnect()
 
                 //Uncomment line below if need to print parsed string to the log
                 //Log.i("STREAMURL:", httpStreamUrl)
@@ -317,9 +301,9 @@ private const val TAG = "PlayerActivity"
                 val myPrev=jsonObject.getString("preview_mp3_128_url")*/
             }
             else {
-                Log.e("Track Streaming Error",responseCode.toString())
-                Log.e("ERROR MESSAGE:",trackConn.responseMessage)
-                trackConn.disconnect()
+                Log.e("Track Streaming Error",responseCodeAudioConnection.toString())
+                Log.e("ERROR MESSAGE:",audioConnectionSC.responseMessage)
+                audioConnectionSC.disconnect()
             }
         }
     }
@@ -333,12 +317,6 @@ private const val TAG = "PlayerActivity"
             .createMediaSource(fromUri("https://firebasestorage.googleapis.com/v0/b/tui-la.appspot.com/o/Guided%20Meditation%20Videos%2Flarge_moon_on_lake_video.mp4?alt=media&token=a5b04e55-0fd1-457c-a9dd-83c0e7265371&_gl=1*dtfdkc*_ga*MTM5MDAzMTAzNi4xNjkyMzIxMjc4*_ga_CW55HF8NVT*MTY5NzEzMzM0NS4xOC4xLjE2OTcxMzUwMzcuNjAuMC4w"))
         val googleVidSource = ProgressiveMediaSource.Factory(dataSourceFactory)
             .createMediaSource(fromUri("https://www.googleapis.com/drive/v3/files/1RMdKtJUQoYA4t1KfycAuq24QizkKemx7?alt=media&key=$Google_Cloud_API_Key"))
-
-        val loopVideo = ConcatenatingMediaSource2.Builder()
-            .add(vidSource, trackDuration.toLong())
-            .add(vidSource, trackDuration.toLong())
-            .add(vidSource, trackDuration.toLong())
-            .build()
 
         val mergeSource = MergingMediaSource(progressiveMediaSource,vidSource)
 
