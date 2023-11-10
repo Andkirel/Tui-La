48ba8943cd31fec50d18e830bf03d2eb0cd6dd87
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2023-11-09 19:09:03 -0600

    Adjusted build.gradle.kts for compatible compose compiler and created beginning shape/icon for the inflating logo animation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.tui_la"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.tui_la"
@@ -35,6 +35,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    composeOptions{
+        kotlinCompilerExtensionVersion = "1.4.1"
+    }
     buildFeatures {
         viewBinding = true
         compose = true
@@ -43,6 +46,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.compose.animation:animation-core-android:1.5.4")
     val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
@@ -52,7 +56,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.compose.ui:ui-tooling:1.5.0")
+    implementation("androidx.compose.ui:ui-tooling:1.5.4")
 
     implementation ("com.google.android.gms:play-services-ads:22.3.0")
     implementation ("androidx.recyclerview:recyclerview-selection:1.1.0")
--- app/src/main/java/com/example/tui_la/GuidedBreathingActivity.kt
+++ app/src/main/java/com/example/tui_la/GuidedBreathingActivity.kt
@@ -1,32 +1,33 @@
 package com.example.tui_la
 
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextClock
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.scaleIn
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 class GuidedBreathingActivity: AppCompatActivity() {
-    private var navigationBackButton = findViewById<ImageButton>(R.id.breathing_back_button)
-    private var inflatableLogo = findViewById<ImageButton>(R.id.breathing_inflatable_logo)
-    private var startStopButton = findViewById<Button>(R.id.button_breathing_start_stop)
-    private var timerNumbers = findViewById<TextClock>(R.id.breathing_timer_clock)
-    private var timerCircle = findViewById<ProgressBar>(R.id.breathing_timer_progress)
-    private var promptText = findViewById<TextView>(R.id.breathing_prompt)
-    private var circleOutline = findViewById<ImageView>(R.id.breathing_circle_outline)
-    
+    //private var navigationBackButton = findViewById<ImageButton>(R.id.breathing_back_button)
+//    private var inflatableLogo = findViewById<ImageButton>(R.id.breathing_inflatable_logo)
+    //private var startStopButton = findViewById<Button>(R.id.button_breathing_start_stop)
+    //private var timerNumbers = findViewById<TextClock>(R.id.breathing_timer_clock)
+    //private var timerCircle = findViewById<ProgressBar>(R.id.breathing_timer_progress)
+    //private var promptText = findViewById<TextView>(R.id.breathing_prompt)
+  //  private var circleOutline = findViewById<ImageView>(R.id.breathing_circle_outline)
+  /*
 
     override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
@@ -36,22 +37,68 @@ class GuidedBreathingActivity: AppCompatActivity() {
 
         }
 
-    }
+    }*/
+   /* @Preview
     @Composable
     fun startBreathingExercise() {
         circleOutline.visibility = View.VISIBLE
-        /*inflatableLogo(
+        *//*inflatableLogo(
             animationSpec = spring(stiffness = Spring.StiffnessMediumLow),
             initialScale = inflatableLogo.scaleX,
             transformOrigin = TransformOrigin.Center
-        ) + expandVertically(expandFrom = Alignment.CenterVertically)*/
+        ) + expandVertically(expandFrom = Alignment.CenterVertically)*//*
         scaleIn(
             animationSpec = spring(stiffness = Spring.StiffnessMediumLow),
             initialScale = inflatableLogo.scaleX,
             transformOrigin = TransformOrigin.Center
-        ) + expandVertically(expandFrom = Alignment.CenterVertically)
-
+        ) + expandVertically(expandFrom = Alignment.CenterVertically)*/
+        /*CircleShape(
+            Image(
+                painterResource(id = R.drawable.logo),
+                modifier = Modifier
+                    .size(100.dp, 175.dp)
+                    .background(Color.Transparent)
+                    .border(
+                        border = BorderStroke(
+                            width = 5.dp,
+                            color = colorResource(id = R.color.cactus_purple)
+                        ), shape = CircleShape
+                    )
+            )
+        )*/
 
     }
-
-}
+    @Preview(showBackground = true)
+    @Composable
+    fun inflateLogo(){
+        Box(modifier = Modifier
+            .size(300.dp,300.dp),
+            ) {
+            Image(
+                painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(150.dp, 150.dp)
+                    .background(Color.Transparent)
+                    .border(
+                        BorderStroke(
+                            width = 4.dp,
+                            colorResource(id = R.color.light_coral), //TODO: Change color to cactus_purple when ready to finish
+                        ), shape = CircleShape
+                    ),
+                Alignment.Center,
+                contentScale = ContentScale.Inside
+            )
+            Image(
+                painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(150.dp, 150.dp)
+                    .background(Color.Transparent),
+                Alignment.Center,
+                contentScale = ContentScale.Inside
+            )
+        }
+    }
