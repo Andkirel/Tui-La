e4aaa713362053509f1e67aa86cdad09f5536a9d
Cody Laframboise <CALaframboise@student.fullsail.edu>
2023-11-11 19:17:37 -0500

    updated back button functionality in the home activity

--- app/src/main/java/com/example/tui_la/HomeActivity.kt
+++ app/src/main/java/com/example/tui_la/HomeActivity.kt
@@ -3,18 +3,19 @@ package com.example.tui_la
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.tui_la.databinding.FragmentcontainerBinding
 
 class HomeActivity : AppCompatActivity(), ICommunicatorHome {
 
     private lateinit var binding: FragmentcontainerBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentcontainerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Display the EmotionsFragment without adding it to the back stack
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainer, EmotionsFragment())
@@ -23,6 +24,9 @@ class HomeActivity : AppCompatActivity(), ICommunicatorHome {
     }
 
     override fun passDataToFragment(uid: String) {
+        // Clear the back stack before adding HomeFragment
+        supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
         val fragment = HomeFragment.newInstance(uid)
         replaceFragment(fragment)
     }
@@ -30,6 +34,17 @@ class HomeActivity : AppCompatActivity(), ICommunicatorHome {
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, fragment)
+            .addToBackStack(null)  // This line is optional, depending on your back stack needs
             .commit()
+    }
+
+    override fun onBackPressed() {
+        // If the HomeFragment is the current fragment, do not pop the back stack
+        if (supportFragmentManager.backStackEntryCount > 1) {
+            super.onBackPressed()
+        } else {
+            // Prevent going back from HomeFragment or close the activity if needed
+            // finish() // Uncomment this if you want to close the activity
+        }
     }
 }
