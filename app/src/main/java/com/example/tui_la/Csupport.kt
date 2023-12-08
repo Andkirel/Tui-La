6f0cab27e5538a6115ba54409e32a896bc6e2cdf
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2023-12-08 16:06:35 -0600

    Changed upper left button on Crisis Support to back button and added click functionality ti go back to main menu

--- app/src/main/java/com/example/tui_la/Csupport.kt
+++ app/src/main/java/com/example/tui_la/Csupport.kt
@@ -1,12 +1,21 @@
 package com.example.tui_la
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tui_la.R
 
 class Csupport: AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_support)
+
+        val Crisis = findViewById<ImageButton>(R.id.Backbutton_Crisis)
+        Crisis.setOnClickListener {
+            val Intent = Intent(this, MainMenu::class.java)
+            startActivity(Intent)
+        }
     }
 }
